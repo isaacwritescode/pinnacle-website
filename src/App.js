b65473625da4d970093c9092fd206ecbfa204b65
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-03 15:39:27 +0530

    add font

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, Typography } from "@mui/material";
+import theme from "./theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1">Circular</Typography>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,59 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root:{
+            fontWeight: 500,
+        },
+        h1: {
+          letterSpacing: "-4px",
+          lineHeight: 1.2,
+          fontSize: "56px",
+        },
+        h2: {
+          letterSpacing: "-4px",
+          lineHeight: 1.1,
+          fontSize: "48px",
+        },
+        h3: {
+          letterSpacing: "-3px",
+          fontSize: "36px",
+        },
+        h4: {
+          letterSpacing: "-2px",
+        },
+        h5: {
+          letterSpacing: "-1px",
+          lineHeight: 1.3,
+        },
+        h6: {
+          letterSpacing: "-0.5px",
+          lineHeight: 1.3,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          borderRadius: 8,
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: "CircularStd-Private",
+  },
+  palette: {
+    mode: "light",
+    primary: {
+      contrastText: "#000",
+      light: "#F8FFF5",
+      main: "#71E83A",
+    },
+  },
+});
+
+export default theme;
