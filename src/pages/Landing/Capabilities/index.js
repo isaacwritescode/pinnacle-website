0c3a4e049fd14ab4edb2fde700dbc428bc2c62c6
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-05 00:12:32 +0530

    fix capabilities images

--- src/pages/Landing/Capabilities/index.js
+++ src/pages/Landing/Capabilities/index.js
@@ -25,76 +25,80 @@ const Capabilities = () => {
   return (
     <Box
       bgcolor={CAPABILITIES[currIdx].bgColor}
-      height={{ xs: "auto", md: "100vh" }}
       overflow="hidden"
-      pt={{ xs: 8, md: 0 }}
       sx={{ transition: "all ease 0.3s" }}
       id="capabilities"
     >
-      <Grid container alignItems="center" columns={{ xs: 6, sm: 12 }}>
-        <Grid item xs={6}>
-          <Stack pl={{ xs: "5vw", md: "15vw" }} spacing={{ xs: 3, md: 4 }}>
-            <Typography variant="h5">Our Capabilities</Typography>
-            <Stack spacing={{ xs: 2, md: 4 }}>
-              {CAPABILITIES.map(({ title }, idx) => (
-                <Typography
-                  variant={md ? "h3" : "h4"}
-                  key={idx}
-                  sx={{
-                    opacity: currIdx === idx ? 1 : 0.4,
-                  }}
-                >
-                  {title}
-                </Typography>
-              ))}
-            </Stack>
-            <Stack direction="row" alignItems="center">
-              <Stack order={currIdx}>
-                <ProgressBar
-                  transition={value >= 1 ? "ease 0.3s" : "none"}
-                  progress={value}
-                  radius={12}
-                  trackStrokeColor="#00000020"
-                  strokeColor="#000000"
-                  strokeWidth={6}
-                  trackStrokeWidth={6}
+      <Stack
+        justifyContent="center"
+        width={{ xs: "90%", md: "70%" }}
+        mx="auto"
+      >
+        <Grid container alignItems="center" py={16} columns={{ xs: 6, sm: 12 }}>
+          <Grid item xs={6}>
+            <Stack spacing={{ xs: 3, md: 4 }}>
+              <Typography variant="h5">Our Capabilities</Typography>
+              <Stack spacing={{ xs: 2, md: 4 }}>
+                {CAPABILITIES.map(({ title }, idx) => (
+                  <Typography
+                    variant={md ? "h3" : "h4"}
+                    key={idx}
+                    sx={{
+                      opacity: currIdx === idx ? 1 : 0.4,
+                    }}
+                  >
+                    {title}
+                  </Typography>
+                ))}
+              </Stack>
+              <Stack direction="row" alignItems="center">
+                <Stack order={currIdx}>
+                  <ProgressBar
+                    transition={value >= 1 ? "ease 0.3s" : "none"}
+                    progress={value}
+                    radius={12}
+                    trackStrokeColor="#00000020"
+                    strokeColor="#000000"
+                    strokeWidth={6}
+                    trackStrokeWidth={6}
+                  />
+                </Stack>
+                <Stack
+                  mx={1}
+                  order={(currIdx + 1) % 4}
+                  width={8}
+                  height={8}
+                  bgcolor="#00000020"
+                  borderRadius={100}
+                />
+                <Stack
+                  mx={1}
+                  order={(currIdx + 2) % 4}
+                  width={8}
+                  height={8}
+                  bgcolor="#00000020"
+                  borderRadius={100}
+                />
+                <Stack
+                  mx={1}
+                  order={(currIdx + 3) % 4}
+                  width={8}
+                  height={8}
+                  bgcolor="#00000020"
+                  borderRadius={100}
                 />
               </Stack>
-              <Stack
-                mx={1}
-                order={(currIdx + 1) % 4}
-                width={8}
-                height={8}
-                bgcolor="#00000020"
-                borderRadius={100}
-              />
-              <Stack
-                mx={1}
-                order={(currIdx + 2) % 4}
-                width={8}
-                height={8}
-                bgcolor="#00000020"
-                borderRadius={100}
-              />
-              <Stack
-                mx={1}
-                order={(currIdx + 3) % 4}
-                width={8}
-                height={8}
-                bgcolor="#00000020"
-                borderRadius={100}
-              />
             </Stack>
-          </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <img
+              src={CAPABILITIES[currIdx].thumbnail}
+              width="100%"
+              alt={CAPABILITIES[currIdx].title}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <img
-            src={CAPABILITIES[currIdx].thumbnail}
-            width="100%"
-            alt={CAPABILITIES[currIdx].title}
-          />
-        </Grid>
-      </Grid>
+      </Stack>
     </Box>
   );
 };
