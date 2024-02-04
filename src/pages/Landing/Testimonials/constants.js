d722a9b45a73990178fcddd2c2e43fdb876cf6d7
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-04 21:15:03 +0530

    5 testimonials

--- src/pages/Landing/Testimonials/constants.js
+++ src/pages/Landing/Testimonials/constants.js
@@ -29,36 +29,7 @@ const TESTIMONIALS = [
     designation: "Designation",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
-  {
-    avatar: "/images/testimonials/placeholder.png",
-    name: "Full name",
-    designation: "Designation",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    avatar: "/images/testimonials/placeholder.png",
-    name: "Full name",
-    designation: "Designation",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    avatar: "/images/testimonials/placeholder.png",
-    name: "Full name",
-    designation: "Designation",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    avatar: "/images/testimonials/placeholder.png",
-    name: "Full name",
-    designation: "Designation",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-  {
-    avatar: "/images/testimonials/placeholder.png",
-    name: "Full name",
-    designation: "Designation",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
+
 ];
 
 
