2e9ecf73c75b090c994ebc5c6cfd8b5a10cfb8e2
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-05 23:46:51 +0530

    add capabilities thumbnails

--- src/pages/Landing/Capabilities/constants.js
+++ src/pages/Landing/Capabilities/constants.js
@@ -1,23 +1,23 @@
 const CAPABILITIES = [
   {
     title: "Web Design & Development",
-    thumbnail: "/images/capabilities/web-development.png",
+    thumbnail: "/images/capabilities/web-dev.png",
     bgColor: "#FADDE4",
   },
   {
     title: "Branding",
-    thumbnail: "/images/capabilities/web-development.png",
+    thumbnail: "/images/capabilities/branding.png",
     bgColor: "#DDF1FA",
   },
   {
     title: "Ecommerce",
-    thumbnail: "/images/capabilities/web-development.png",
-    bgColor: "#DDFAF3",
+    thumbnail: "/images/capabilities/ecommerce.png",
+    bgColor: "#FAF4DD",
   },
   {
     title: "Digital Marketting",
-    thumbnail: "/images/capabilities/web-development.png",
-    bgColor: "#FAF4DD",
+    thumbnail: "/images/capabilities/digital-marketting.png",
+    bgColor: "#F4EAF4",
   },
 ];
 
