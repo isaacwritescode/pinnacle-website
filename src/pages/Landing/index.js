79ebe3e3c6389a9771e2e1578c1b52c0760228d8
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-05 13:35:14 +0530

    add FAB

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -5,17 +5,60 @@ import Sponsors from "./Sponsors";
 import FAQ from "./FAQ";
 import Testimonials from "./Testimonials";
 import Work from "./Work";
+import { Box, Button } from "@mui/material";
+import ReactVisibilitySensor from "react-visibility-sensor";
+import Footer from "../../components/Footer";
+import { useState } from "react";
 
 const Landing = () => {
+  const [heroVisible, setHeroVisible] = useState(true);
+  const [footerVisible, setFooterVisible] = useState(false);
+  // console.log({
+  //   heroVisible,
+  //   footerVisible,
+  // });
+  const condition = !(heroVisible || footerVisible);
+
   return (
     <>
-      <Hero />
+      <ReactVisibilitySensor
+        onChange={(isVisible) => {
+          setHeroVisible(isVisible);
+        }}
+        delayedCall
+        partialVisibility
+      >
+        <Hero />
+      </ReactVisibilitySensor>
       <Sponsors />
       <Approach />
       <Capabilities />
       <Work />
       <FAQ />
       <Testimonials />
+      <ReactVisibilitySensor
+        onChange={(isVisible) => {
+          setFooterVisible(isVisible);
+        }}
+        delayedCall
+        partialVisibility
+      >
+        <Footer />
+      </ReactVisibilitySensor>
+
+      <Box
+        className={condition ? "fade-in" : "fade-out"}
+        sx={{
+          position: "fixed",
+          bottom: 24,
+          right: 24,
+          zIndex: 99,
+        }}
+      >
+        <Button variant="contained" size="large">
+          Start a project
+        </Button>
+      </Box>
     </>
   );
 };
