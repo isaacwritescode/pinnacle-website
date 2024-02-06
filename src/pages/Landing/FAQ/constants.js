16ac2c3a3e3262dc0166c4d699ba2a4f3b3a1bb4
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-06 23:00:44 +0530

    add faq content

--- src/pages/Landing/FAQ/constants.js
+++ src/pages/Landing/FAQ/constants.js
@@ -1,22 +1,22 @@
 export const FAQ_ITEMS = [
   {
-    title: "Why go with Pinnacle?",
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    title: "What services does your agency provide?",
+    desc: "We help businesses grow by crafting strategies to attract, engage, and retain customers. We use various tools like social media, advertising, SEO, and content creation to increase brand visibility and drive sales.",
   },
   {
-    title: "Do you possess the knowledge to help me with my project?",
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    title: "How can a marketing agency benefit my small business?",
+    desc: "Hiring a marketing agency allows you to tap into expertise without the overhead of maintaining an in-house team. We bring fresh perspectives, specialized skills, and dedicated focus to elevate your brand, attract new customers, and increase revenue, ultimately helping your business thrive.",
   },
   {
-    title: "Do you also provide a creative concept for videos and images?",
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    title: "Can your agency work within my budget constraints?",
+    desc: "Absolutely! We understand the importance of budgeting especially for small businesses. Our approach is flexible, and we tailor our services to align with your budget while maximizing your return on investment (ROI). Whether you're just starting or looking to scale, we'll work with you to achieve your marketing goals cost-effectively.",
   },
   {
-    title: "How can I get a proposal?",
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    title: "How long does it take to see results from marketing efforts?",
+    desc: "The timeline for seeing results varies depending on factors like your industry, competition, and the strategies implemented. While some tactics yield immediate results, others, such as SEO and content marketing, may take longer to show significant impact. Rest assured, we'll provide transparent communication and regular progress updates to keep you informed every step of the way.",
   },
   {
-    title: "Do you provide only design for my project, or development as well?",
-    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    title: "What sets Pinnacle apart from other agencies?",
+    desc: "We prioritize personalized attention and collaboration. We take the time to understand your unique business goals, target audience, and competitive landscape to develop tailored strategies that deliver results. Additionally, our team is dedicated to staying ahead of industry trends and continuously refining our approach to ensure your business stays ahead of the curve in today's dynamic marketplace.",
   },
 ];
