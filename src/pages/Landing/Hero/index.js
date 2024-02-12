055f2d94c9768a97cba5ae5fd167f17af10b5145
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-02-13 00:02:28 +0530

    fix parallax

--- src/pages/Landing/Hero/index.js
+++ src/pages/Landing/Hero/index.js
@@ -15,20 +15,16 @@ const Hero = () => {
   const md = useMediaQuery(theme.breakpoints.up("md"));
   return md ? (
     <Box>
-      <ParallaxProvider>
-        <Box position="absolute" top={0} zIndex={-1}>
-          <img src="/images/hero/bg.svg" alt="background" width="100%" />
-        </Box>
-        <Box position="fixed" top={-120} zIndex={-1}>
-          <Parallax speed={-10}>
-            <img
-              src="/images/hero/mountains.svg"
-              alt="mountain with hills"
-              width="100%"
-            />
-          </Parallax>
-        </Box>
-      </ParallaxProvider>
+      <Box position="absolute" top={0} zIndex={-1}>
+        <img src="/images/hero/bg.svg" alt="background" width="100%" />
+      </Box>
+      <Box position="fixed" top={-120} zIndex={-1}>
+        <img
+          src="/images/hero/mountains.svg"
+          alt="mountain with hills"
+          width="100%"
+        />
+      </Box>
       <Box pt={32} pb={48} zIndex={99} position="relative">
         <Box position="absolute" bottom={-12} zIndex={-1}>
           <img
@@ -38,28 +34,32 @@ const Hero = () => {
           />
         </Box>
         <Box width={{ xs: "90%", md: "70%" }} mx="auto">
-          <Stack
-            alignItems="center"
-            spacing={2}
-            maxWidth={600}
-            mx="auto"
-            textAlign="center"
-          >
-            <Typography variant="h2" color="white">
-              Reach your pinnacle
-            </Typography>
-            <Typography variant="body1" color="white">
-              We're passionate about helping businesses reach their peak
-              potential, and we're confident that we can make it{" "}
-            </Typography>
-            <Box pt={4} data-sal="zoom-out" data-sal-delay="500">
-              <Link href="/contact">
-                <Button variant="contained" size="medium">
-                  Start a project
-                </Button>
-              </Link>
-            </Box>
-          </Stack>
+          <ParallaxProvider>
+            <Parallax speed={-50}>
+              <Stack
+                alignItems="center"
+                spacing={2}
+                maxWidth={600}
+                mx="auto"
+                textAlign="center"
+              >
+                <Typography variant="h2" color="white">
+                  Reach your pinnacle
+                </Typography>
+                <Typography variant="body1" color="white">
+                  We're passionate about helping businesses reach their peak
+                  potential, and we're confident that we can make it{" "}
+                </Typography>
+                <Box pt={4} data-sal="zoom-out" data-sal-delay="500">
+                  <Link href="/contact">
+                    <Button variant="contained" size="medium">
+                      Start a project
+                    </Button>
+                  </Link>
+                </Box>
+              </Stack>
+            </Parallax>
+          </ParallaxProvider>
         </Box>
       </Box>
     </Box>
